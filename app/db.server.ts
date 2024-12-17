c301566a9b4a6b68c6ea6032cfa85aa29a6db6bf
Chanakan Mungtin <chanakan5591@chanakancloud.net>
2024-12-17 09:30:56 +0700

    fixed

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,12 +1,5 @@
 import { PrismaClient } from '@prisma/client'
-import { withAccelerate } from '@prisma/extension-accelerate'
 import { env } from './env'
 
-const prisma = new PrismaClient({
-    datasources: {
-        db: {
-            url: env.DATABASE_URL,
-        },
-    },
-}).$extends(withAccelerate())
+const prisma = new PrismaClient()
 export default prisma
--- app/env.ts
+++ app/env.ts
@@ -3,8 +3,7 @@ import { z } from 'zod'
 
 export const env = createEnv({
     server: {
-        DATABASE_URL: z.string().url().startsWith("prisma://"), // enforce use of Prisma Accelerate
-        DIRECT_DATABASE_URL: z.string().url()
+        DATABASE_URL: z.string().url(),
     },
     clientPrefix: "VITE_",
     client: {
