461c0697d2f63d0600939a64c3a262e125454a97
Chanakan Mungtin <chanakan5591@chanakancloud.net>
2025-01-30 17:25:27 +0700

    fixed typo

--- app/root.tsx
+++ app/root.tsx
@@ -105,7 +105,7 @@ export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
         ? "The requested page could not be found."
         : error.statusText || details;
   } else if (error && error instanceof Error) {
-    Sentry.captureException(error)Sentry.captureException(error);;
+    Sentry.captureException(error);
     if (import.meta.env.DEV) {
       details = error.message;
       stack = error.stack;
